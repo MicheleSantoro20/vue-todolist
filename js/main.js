eaf921d9819ba57d89049986ea1a01b6efe04fe2
Michele Santoro <michelesantoro2000@icloud.com>
2023-03-17 03:01:23 +0100

    Esercizio concluso

--- js/main.js
+++ js/main.js
@@ -3,7 +3,40 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        newTask:"",
+        taskList:[
+          {
+            text:'mansione 1',
+            done:false
+          },
+          {
+            text:'mansione 2',
+            done:true
+          },
+          {
+            text:'mansione 3',
+            done:false
+          },
+        ]
+      }
+    },
+    methods:{
+      removeTask(index){
+        this.taskList.splice(index,1);
+      },
+      addTask(){
+        if(this.newTask.length > 0){
+          this.taskList.push({text:this.newTask,done:false});
+          this.newTask="";
+        }
+      },
+      checker(element){
+        if(element.done==false){
+          element.done = true;
+        }else{
+          element.done = false;
+        }
+        
       }
     }
   }).mount('#app')
